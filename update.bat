f9937ac8436f62decdf84673bf15f1709a6ec7e5
JacobPickens <pickuptruck2000@gmail.com>
2026-02-05 04:02:00 -0600

    QOL

--- update.bat
+++ update.bat
@@ -1,3 +1,12 @@
 git pull
 
+@echo off
+
+echo           WAIT! One more step.
+echo ===========================================
+echo   1. Open chrome -> manage extensions
+echo   2. Make sure developer mode is enabled
+echo   3. Click 'Load Unpacked' to the left
+echo   4. Go to cssass/extension and select it
+echo ===========================================
 pause
